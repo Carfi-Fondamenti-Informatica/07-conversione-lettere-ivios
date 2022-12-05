16b9570e43df9ddebed60f1e48a0c1e8df0f9fb7
Ivio Feng <115487784+ivios@users.noreply.github.com>
2022-12-05 10:12:54 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+bool lettera (char &a) {
+    bool flag = false;
+    if (((a>=65)and(a<=90))or((a>=97)and(a<=122))){
+        flag = true;
+        if ((a>=65)and(a<=90)){
+            a += 32;
+        } else {
+            a -= 32;
+        }
+    }
+    return flag;
+}
